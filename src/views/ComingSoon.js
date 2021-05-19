a2be3b1c459cf3d7d7b7ac5a138bbc02894abc3f
amohdsharfuddin <amohdsharfuddin@guidewire.com>
2021-05-19 16:46:38 +0800

    DKIP-52 Develop Maintanance Page and Add Icon

--- src/views/ComingSoon.js
+++ src/views/ComingSoon.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Container } from "shards-react";
 
 import BackButton from "../components/common/BackButton";
-
+import StopwatchIcon from "../images/general/icon/stopwatch-coming-soon-icon";
 const ComingSoon = () => (
   <Container fluid className="main-content-container px-4 pb-4">
     <div className="error">
       <div className="error__content">
+        <StopwatchIcon height={150} width={150} />
+
         <h1>Coming Soon</h1>
         <h2>Stay tuned! </h2>
         <p>New content is around the corner.</p>
