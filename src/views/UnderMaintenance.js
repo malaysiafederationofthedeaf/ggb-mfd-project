25c9279323cf721582e9aa80d0c217aa936b6fab
amohdsharfuddin <amohdsharfuddin@guidewire.com>
2021-05-19 16:46:38 +0800

    DKIP-52 Develop Maintanance Page and Add Icon

--- src/views/UnderMaintenance.js
+++ src/views/UnderMaintenance.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Container } from "shards-react";
 
 import BackButton from "../components/common/BackButton";
+import ToolsIcon from "../images/general/icon/tools-maintenance-icon";
 
 const UnderMaintenance = () => (
   <Container fluid className="main-content-container px-4 pb-4">
     <div className="error">
       <div className="error__content">
+        <ToolsIcon height={150} width={150} />
         <h1>Under Maintenance</h1>
         <h2>We'll be back!</h2>
         <p>
