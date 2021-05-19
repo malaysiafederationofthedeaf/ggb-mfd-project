7861167c18746a9c94167fa111cdb4d627b6b82f
amohdsharfuddin <amohdsharfuddin@guidewire.com>
2021-05-19 17:38:33 +0800

    Revert "Update data structure"
    This reverts commit 819385d21a9631429d7ffeab8f486c05d04ee77c.

--- src/data/categories/readExcel.js
+++ src/data/categories/readExcel.js
@@ -15,13 +15,14 @@ const fetchData = async (url) => {
 
 const restructureJSON = (data) => {
   const reconData = data.map((item) => ({
-    group: item.Group,
-    kumpulan: item.Kumpulan,
-    category: item.Category,
-    kategori: item.Kategori,
-    word: item.Word,
-    perkataan: item.Perkataan,
-    video: item.Video,
+    Group: item.Group,
+    Kumpulan: item.Kumpulan,
+    Category: item.Category,
+    Kategori: item.Kategori,
+    Word: item.Word,
+    Perkataan: item.Perkataan,
+    Image: item.Image,
+    Video: item.Video,
   }));
 
   return reconData;
