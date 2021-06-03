e03d2c6cc6a959527714ea52a191f233d56ae828
Jing Wen Liow <jliow@guidewire.com>
2021-06-03 13:06:18 +0800

    DKIP-83 BIM Homepage Cosmetic Corrections

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -2,21 +2,16 @@ export default function() {
   return [
     {
       title: "nav_item_1_title",
-      htmlBefore: '<i class="material-icons">home</i>',
-      to: "/home",
-    },
-    {
-      title: "nav_item_2_title",
       htmlBefore: '<i class="material-icons">info</i>',
       to: "/about-us",
     },
     {
-      title: "nav_item_3_title",
+      title: "nav_item_2_title",
       htmlBefore: '<i class="material-icons">category</i>',
       to: "/groups",
     },
     {
-      title: "nav_item_4_title",
+      title: "nav_item_3_title",
       htmlBefore: '<i class="material-icons">alphabet</i>',
       to: "/alphabets",
     },
