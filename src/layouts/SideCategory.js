febd83ba4720600365b8ad42ae795fc4ed3abe37
jliow <81293289+jliow@users.noreply.github.com>
2021-06-04 12:07:04 +0800

    DKIP-84 BIM List Page Cosmetic Corrections (#30)
    * DKIP-84 BIM List Page Cosmetic Corrections
    
    * DKIP-84 BIM List Page Cosmetic Corrections
    
    * DKIP-84 BIM List Page Cosmetic Corrections

--- src/layouts/SideCategory.js
+++ src/layouts/SideCategory.js
@@ -22,13 +22,13 @@ const SideCategoryLayout = ({ children, noNavbar, noFooter }) => {
       <Row>
         <Col className="main-content p-0" tag="main">
           {!noNavbar && <MainNavbar toggle={toggleDropdown} />}
-          <Row>
-            <Col xl="2" lg="3" md="3" sm="4">
+          <Row className="side-layout">
+            <Col xl="2" lg="3" md="3" className="p-0">
               <div className="sidebar-category-wrapper">
                 <SidebarCategory urlParam={alphabet} />
               </div>
             </Col>
-            <Col xl="10" lg="9" md="9" sm="8">
+            <Col xl="10" lg="9" md="9" sm="12">
               {children}
             </Col>
           </Row>
