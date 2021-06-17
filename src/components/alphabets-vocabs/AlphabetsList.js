0ca5ea71b7572576a93e63fa3f859b4bc2c51a1c
Jing Wen Liow <jliow@guidewire.com>
2021-06-17 15:51:54 +0800

    DKIP-105 R1 - Cosmetic - Sign Language image stretched

--- src/components/alphabets-vocabs/AlphabetsList.js
+++ src/components/alphabets-vocabs/AlphabetsList.js
@@ -19,13 +19,13 @@ const AlphabetsList = ({ vocabs, alphabet }) => {
                 data-aos-delay="200"
                 className="vocab-word"
               >
-                <Col xs="5" sm="5" md="5" lg="5">
+                <Col sm="3" md="4" lg="4" className="pl-2 pr-0">
                   <VocabWordPerkataan
                     word={vocab.word}
                     perkataan={vocab.perkataan}
                   />
                 </Col>
-                <Col xs="7" sm="7" md="7" lg="7" className="vocab-image-wrapper">
+                <Col sm="9" md="8" lg="8" className="vocab-image-wrapper">
                   <img
                     src={vocabImgSrc}
                     alt={vocab.word}
