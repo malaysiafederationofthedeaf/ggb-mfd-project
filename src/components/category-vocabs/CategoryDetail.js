48d6eb905969dba3237c7d4f6168c87f96cbb09d
jliow <81293289+jliow@users.noreply.github.com>
2021-06-21 18:00:29 +0800

    DKIP-110 R1 - Cosmetic - Loading card transition (#52)

--- src/components/category-vocabs/CategoryDetail.js
+++ src/components/category-vocabs/CategoryDetail.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Card, CardBody, CardTitle } from "shards-react";
 import { useTranslation } from "react-i18next";
+import styled, { keyframes } from 'styled-components';
+import { zoomIn } from 'react-animations';
 
 import { Store } from "../../flux";
 
@@ -12,15 +14,17 @@ const CategoryDetail = ({ categoryItem, group }) => {
   const groupFormatted = Store.formatString(group);
   const categoryFormatted = Store.formatString(categoryItem.category);
 
+  const ZoomIn = styled.div`animation: .5s ${keyframes `${zoomIn}`}`;  
+
   return (
     <Link to={`/groups/${groupFormatted}/${categoryFormatted}`}>
       <Card small className="card-post card-post--1">
-        <div
-          className="card-post__image"
-          data-aos="zoom-in"
-          data-aos-delay="200"
-          style={{ backgroundImage: `url('${categoryImgSrc}')` }}
-        ></div>
+        <ZoomIn>
+          <div
+            className="card-post__image"
+            style={{ backgroundImage: `url('${categoryImgSrc}')` }}
+          ></div>
+        </ZoomIn>
         <CardBody>
           <CardTitle className="card-title">{t(categoryFormatted)}</CardTitle>
         </CardBody>
