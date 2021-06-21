d0b9c795eebd64daea44d19f206b589de10bbc16
amohdsharfuddin <amohdsharfuddin@guidewire.com>
2021-06-21 14:17:56 +0800

    DKIP-107 Enhancement on Week/Month Category

--- src/components/layout/MainNavbar/NavbarTranslate.js
+++ src/components/layout/MainNavbar/NavbarTranslate.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import i18next from "i18next";
-import {
-  Button,
-} from "shards-react";
+import { Button } from "shards-react";
 import "flag-icon-css/css/flag-icon.min.css";
 import { Store } from "../../../flux";
 
@@ -10,26 +8,27 @@ const languages = Store.getLanguages();
 const countryCode = Store.getCountryCode();
 
 const NavbarTranslate = () => {
-  const [flag, setFlag] = useState(false)
+  const [flag, setFlag] = useState(false);
 
   const toggleLocale = () => {
-    setFlag(!flag)
-    if(flag)
-    // English
-      i18next.changeLanguage(languages[0])
-    else
+    setFlag(!flag);
+    if (flag)
+      // English
+      i18next.changeLanguage(languages[0]);
     // Malay
-      i18next.changeLanguage(languages[1])
-  }
-  
+    else i18next.changeLanguage(languages[1]);
+  };
 
   return (
     <div className="navbar-translate-btn">
       <Button onClick={toggleLocale}>
         <span
-          className={`flag-icon flag-icon-${!flag ? countryCode[0] : countryCode[1]} p-1`}
-        ></span></Button>
-      </div>
+          className={`flag-icon flag-icon-${
+            flag ? countryCode[1] : countryCode[0]
+          } p-1`}
+        ></span>
+      </Button>
+    </div>
   );
 };
 
