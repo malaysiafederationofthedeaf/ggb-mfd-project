19e164e77df682573f046108188dc07b6b28c799
sinwaihoe-gw <85343933+sinwaihoe-gw@users.noreply.github.com>
2021-06-22 10:18:36 +0800

    Solve merge conflicts

--- src/components/category-vocabs/CategoryDetail.js
+++ src/components/category-vocabs/CategoryDetail.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Card, CardBody, CardTitle } from "shards-react";
 import { useTranslation } from "react-i18next";
+import styled, { keyframes } from 'styled-components';
+import { zoomIn } from 'react-animations';
 import VocabWordPerkataan from "./VocabWordPerkataan";
 
 import { Store } from "../../flux";
@@ -15,15 +17,17 @@ const CategoryDetail = ({ categoryItem, group }) => {
   const linkToPath = categoryItem.new ? `${basePath}/${Store.formatString(categoryItem.word)}` : `${basePath}/${categoryFormatted}`;
   const imgSrc = categoryItem.new ? Store.getSignImgSrc(categoryItem.perkataan) : Store.getCategoryImgSrc(categoryItem.kategori);
 
+  const ZoomIn = styled.div`animation: .5s ${keyframes `${zoomIn}`}`;  
+
   return (
     <Link to={linkToPath}>
       <Card small className="card-post card-post--1">
-        <div
-          className="card-post__image"
-          data-aos="zoom-in"
-          data-aos-delay="200"
-          style={{ backgroundImage: `url('${imgSrc}')` }}
-        ></div>
+        <ZoomIn>
+          <div
+            className="card-post__image"
+            style={{ backgroundImage: `url('${imgSrc}')` }}>
+            </div>
+        </ZoomIn>
         <CardBody>
           <CardTitle className="card-title">
             {
