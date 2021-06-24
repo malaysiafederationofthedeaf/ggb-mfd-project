dc9d8aba2c090491d498c28fad2864b4288c9ea5
jliow <81293289+jliow@users.noreply.github.com>
2021-06-24 09:42:43 +0800

    DKIP-118 R1 - UX - Page Not Landing/Loading At The Top (#57)
    * DKIP-118 R1 - UX - Page Not Landing/Loading At The Top
    
    * DKIP-118 R1 - UX - Page Not Landing/Loading At The Top
    
    * DKIP-118 R1 - UX - Page Not Landing/Loading At The Top
    
    * DKIP-118 R1 - UX - Page Not Landing/Loading At The Top

--- src/App.js
+++ src/App.js
@@ -8,32 +8,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/App.scss";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 function App() {
   Aos.init();
   return (
-    <Router basename={process.env.REACT_APP_BASENAME || ""}>
-      <div>
-        <Switch>
-          {routes.map((route, index) => {
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={withTracker((props) => {
-                  return (
-                    <route.layout {...props}>
-                      <route.component {...props} />
-                    </route.layout>
-                  );
-                })}
-              />
-            );
-          })}
-        </Switch>
-      </div>
-    </Router>
+      <Router basename={process.env.REACT_APP_BASENAME || ""}>
+          <div>
+          <ScrollToTop>
+            <Switch>
+              {routes.map((route, index) => {
+                return (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    exact={route.exact}
+                    component={withTracker((props) => {
+                      return (
+                        <route.layout {...props}>
+                          <route.component {...props} />
+                        </route.layout>
+                      );
+                    })}
+                  />
+                );
+              })}
+            </Switch>
+            </ScrollToTop>
+          </div>
+        </Router>
   );
 }
 
--- src/components/category-vocabs/VocabDetail.js
+++ src/components/category-vocabs/VocabDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListGroup, ListGroupItem, Col, Row } from "shards-react";
+import { Col, Row } from "shards-react";
 import ReactPlayer from 'react-player';
 
 import { Store } from "../../flux";
@@ -9,35 +9,33 @@ const VocabDetail = ({vocab}) => {
     const vocabImgSrc = Store.getSignImgSrc(vocab.perkataan);
 
     return (
-        <ListGroup flush className="py-2">    
-            <ListGroupItem className="p-0">
-                <Row className="selected-vocab-title">
-                    <Col>
-                        <VocabWordPerkataan word={vocab.word} perkataan={vocab.perkataan} />
-                    </Col>
-                </Row>
-                <Row>        
-                    <Col lg="12" md="12" sm="12">
-                        <div className="selected-vocab-image-wrapper">
-                            <img src={vocabImgSrc} alt={vocab.word} className="selected-vocab-image" />
-                        </div>
-                    </Col>                                
-                    <Col lg="12" md="12" sm="12" >
-                        <div>
-                            {vocab.video === undefined ? 
-                                // if there is no video url
-                                <div className="selected-vocab-image-wrapper">     
-                                    <img src={require(`../../images/general/video-coming-soon.jpg`)} alt={vocab.word}className="selected-vocab-image" />
-                                </div> :
-                                <div className="selected-vocab-video-wrapper">
-                                    <ReactPlayer url={vocab.video} playing={true} controls={true} loop={true} width="100%"/>
-                                </div>
-                            }
-                        </div>
-                    </Col>                    
-                </Row>
-            </ListGroupItem>
-        </ListGroup>
+        <>
+            <Row className="selected-vocab-title">
+                <Col>
+                    <VocabWordPerkataan word={vocab.word} perkataan={vocab.perkataan} />
+                </Col>
+            </Row>
+            <Row>        
+                <Col lg="12" md="12" sm="12">
+                    <div className="selected-vocab-image-wrapper">
+                        <img src={vocabImgSrc} alt={vocab.word} className="selected-vocab-image" />
+                    </div>
+                </Col>                                
+                <Col lg="12" md="12" sm="12" >
+                    <div>
+                        {vocab.video === undefined ? 
+                            // if there is no video url
+                            <div className="selected-vocab-image-wrapper">     
+                                <img src={require(`../../images/general/video-coming-soon.jpg`)} alt={vocab.word}className="selected-vocab-image" />
+                            </div> :
+                            <div className="selected-vocab-video-wrapper">
+                                <ReactPlayer url={vocab.video} playing={true} controls={true} loop={true} width="100%"/>
+                            </div>
+                        }
+                    </div>
+                </Col>                    
+            </Row>
+        </>
     );
 }
 
