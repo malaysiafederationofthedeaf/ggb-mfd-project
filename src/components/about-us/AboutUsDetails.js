917b786c97751b5472602232583560c0fca8362c
jliow <81293289+jliow@users.noreply.github.com>
2021-06-28 17:30:16 +0800

    DKIP-81 "About BIM Project" for MFD & GGB (#63)
    This PR is merged first by the Request of the Product Owner (Don) due to R2 Testing commencing

--- src/components/about-us/AboutUsDetails.js
+++ src/components/about-us/AboutUsDetails.js
@@ -3,38 +3,104 @@ import PropTypes from "prop-types";
 
 import { Container, Row, Col } from "shards-react";
 
-const AboutUsDetails = ({ mfdDetails, t }) => (
-  <div className="about-us d-flex align-items-center">
-    <Container>
-      <Row>
-        <Col lg="6" className="d-flex flex-column justify-content-center">
-          <h1 data-aos="fade-up">{t(mfdDetails.name)}</h1>  
-          <p data-aos="fade-up" data-aos-delay="400">{t(mfdDetails.metaValue1)}</p>         
-          <p data-aos="fade-up" data-aos-delay="500">{t(mfdDetails.metaValue2)}</p> 
+const AboutUsDetails = ({ aboutUsDetails, t }) => 
+<Container>
+<Row>
+  {aboutUsDetails.map((vocab, key) => {
+    return (
+      <div className="about-us d-flex align-items-center">
+        <Col className="d-flex flex-column">
+          {/* logo & title */}
+          <Row className="about-us-title">
+            <Col lg="2" md="12" sm="12">
+              <div className="about-us-title-logo">
+                <img src={vocab.logo} alt={vocab.name} />
+              </div>
+            </Col>
+            <Col lg="10" md="12" sm="12">
+              <h1 className="about-us-title-text" data-aos="fade-up">{t(vocab.metaTitle)}</h1>
+            </Col>
+          </Row>
+
+          {/* paragraph texts */}
+          {vocab.metaValue.map((paragraph, key) =>
+            <p data-aos="fade-up" data-aos-delay="400">{t(paragraph)}</p>         
+          )} 
+
+          {/* for more */}
+          <p data-aos="fade-up" data-aos-delay="400">
+            {t(vocab.forMore)}
+            <a href={vocab.forMoreLink} target="_blank" rel="noopener noreferrer" key={key}>
+              {vocab.forMoreLink}
+            </a>
+            .
+          </p>                           
         </Col>
-        <Col lg="6" className="about-us-img" data-aos="zoom-out" data-aos-delay="200">
-          <img src={mfdDetails.logo} alt={t(mfdDetails.name)}className="img-fluid"/>
-        </Col>
-      </Row>     
-    </Container>
-  </div> 
-);
+      </div> 
+    )
+  })}
+            </Row>     
+        </Container>
+
 
 AboutUsDetails.propTypes = {
   /**
    * The user details object.
    */
-  mfdDetails: PropTypes.object,
+  aboutUsDetails: PropTypes.array,
 };
 
 AboutUsDetails.defaultProps = {
-  mfdDetails: {
-    name: "mfd_name",
-    logo: require("./../../images/mfd/mfd-about.jpg"),
-    metaTitle: "meta_title",
-    metaValue1: "meta_value_1",
-    metaValue2: "meta_value_2",
-  },
+  aboutUsDetails: [
+    {
+      name: "bim_name",
+      logo: require("./../../images/bim/logo/bim-logo.jpg"),
+      image: require("./../../images/mfd/mfd-about.jpg"),
+      metaTitle: "about_bim_title",
+      metaValue: [ 
+        "about_bim_1",
+        "about_bim_2",
+        "about_bim_3",
+        "about_bim_4",
+      ],
+      forMore: "about_bim_more",
+      forMoreLink: "https://infomfd.wixsite.com/website-5/malaysia-sign-language"
+    },    
+    {
+      name: "mfd_name",
+      logo: require("./../../images/mfd/mfd-logo.jpg"),
+      image: require("./../../images/mfd/mfd-kids.jpg"),
+      metaTitle: "about_mfd_title",
+      metaValue: [ 
+        "about_mfd_1", 
+        "about_mfd_2", 
+      ],   
+      forMore: "about_mfd_more",
+      forMoreLink: "https://www.mymfdeaf.org"
+    },
+    {
+      name: "ggb_name",
+      logo: require("./../../images/ggb/ggb-logo-1.jpg"),
+      metaTitle: "about_ggb_title",
+      metaValue: [ 
+        "about_ggb_1", 
+        "about_ggb_2", 
+      ],
+      forMore: "about_ggb_more",
+      forMoreLink: "https://careers.guidewire.com/guidewire-gives-back"
+    },
+    {
+      name: "vercel_name",
+      logo: require("./../../images/general/logo/vercel-logo.jpg"),
+      metaTitle: "about_vercel_title",
+      metaValue: [ 
+        "about_vercel_1", 
+        "about_vercel_2", 
+      ],
+      forMore: "about_vercel_more",
+      forMoreLink: "https://vercel.com/"
+    },        
+  ],
 };
 
 export default AboutUsDetails;
