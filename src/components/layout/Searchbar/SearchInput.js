3f6fd779b6059731d028c4bd49c86735076bb958
Jing Wen Liow <jliow@guidewire.com>
2021-07-12 15:26:35 +0800

    DKIP-141 Search Field Not Reset

--- src/components/layout/Searchbar/SearchInput.js
+++ src/components/layout/Searchbar/SearchInput.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Select, { components } from "react-select";
 import { useHistory } from "react-router-dom";
