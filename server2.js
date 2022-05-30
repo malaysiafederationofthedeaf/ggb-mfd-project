d0c24a75369802a2b709e8649aeb494a03b5cd05
luqmansatay <mhisham@guidewire.com>
2022-05-30 15:06:24 +0800

    added image server

---
+++ server2.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.static('./src/images/bim/vocab'));
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './src/images/bim/vocab')
+    },
+    filename: (req, file, cb) => {
+        cb(null,file.originalname)
+    }
+});
+
+
+const upload = multer({storage}).array('file');
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            return res.status(500).json(err)
+        }
+
+        return res.status(200).send(req.files)
+    })
+});
+
+app.listen(8001, () => {
+    console.log('App is running on port 8001')
+});
