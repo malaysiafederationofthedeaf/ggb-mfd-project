6b4121b725df367fb986f6ae9904486054a1c832
luqmansatay <mhisham@guidewire.com>
2022-05-31 15:11:39 +0800

    Remove the 8000 port from link

--- src/src-admin/views/ExcelUploader/index.js
+++ src/src-admin/views/ExcelUploader/index.js
@@ -239,8 +239,8 @@ export const ExcelUploader = ({onSuccess}) => {
                 for(let i = 0; i < files.length; i++) {
                     data.append('file', files[i]);
                 }
-                console.log(window.location.protocol + '//' + window.location.host +':8000/upload')    
-                axios.post(window.location.protocol +'//' + window.location.hostname + ':8000/upload', data,
+                console.log(window.location.protocol + '//' + window.location.host +'/upload')    
+                axios.post(window.location.protocol +'//' + window.location.hostname + '/upload', data,
                 {headers:
                     {
                         'Content-Disposition': "attachment;",
