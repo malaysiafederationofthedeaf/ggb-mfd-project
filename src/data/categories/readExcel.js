d73755b5130e7136676674a64843a26982518d25
AnasUpm <muhammadanasmarz@gmail.com>
2025-04-18 23:51:25 +0800

    switching endpoint

--- src/data/categories/readExcel.js
+++ src/data/categories/readExcel.js
@@ -8,7 +8,7 @@ const fetchAllData = async () => {
 
   while (hasMoreData) {
     try {
-      const response = await axios.get(`https://my-excel-cms.onrender.com/api/alphabet-entries?pagination[page]=${page}&pagination[pageSize]=25`);
+      const response = await axios.get(`https://mfd-cms-test.onrender.com//api/alphabet-entries?pagination[page]=${page}&pagination[pageSize]=25`);
       
       const { data, meta } = response.data;
       allData = [...allData, ...data];
