2fc1b0c68d2438321937c22e08f3d007ce5dc5f8
Alice2333333333 <alicelau0080@gmail.com>
2025-05-29 11:58:12 +0800

    Fix DKIP-677

--- src/components/category-vocabs/GroupDetail.js
+++ src/components/category-vocabs/GroupDetail.js
@@ -40,7 +40,7 @@ const GroupDetail = ({ category, group }) => {
                 ></div>
               </ZoomIn>
             </Col>
-            <Col lg="6" md="6" sm="6">
+            <Col xs="6" lg="6" md="6" sm="6">
               <CardBody>
                 <h5 className="card-title">{t(isMalay ? category.kategori : category.category)}</h5>
               </CardBody>
