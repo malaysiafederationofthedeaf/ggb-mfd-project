0195351f07a384f435b06d21089c359e89810437
Yi Hern Chan <ychan@guidewire.com>
2026-04-01 16:55:31 +0800

    modified imageURL in store.js

--- src/flux/store.js
+++ src/flux/store.js
@@ -16,7 +16,7 @@ let _store = {
   featuredVideosPlaylistId: "PLEztM-ga58Y4s6t5pac5uJKLeSSuspioQ",
   youtubeAPIKey: "AIzaSyBIk86nsIH0h4HSEgHPLI8bku6WKQlizDk",
   featuredVideos: [],
-  imageURL: "https://pub-484eefc799ec44daac47eef319579772.r2.dev/mfd-signbank-images/",
+  imageURL: "https://pub-484eefc799ec44daac47eef319579772.r2.dev/",
 };
 
 class Store extends EventEmitter {
